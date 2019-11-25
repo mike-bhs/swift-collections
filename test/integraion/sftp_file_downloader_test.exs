86fd135c189901c59f62cfb768594443f9ff3fd0
Mike <mbohoslavskyi@n-ix.com>
2019-11-25 15:01:08 +0200

    Add more tests for SFTP

--- test/integraion/sftp_file_downloader_test.exs
+++ test/integraion/sftp_file_downloader_test.exs
@@ -1,6 +1,7 @@
 defmodule SwiftCollections.SftpFileDownloaderTest do
   use SwiftCollections.RepoCase
 
+  alias SwiftCollections.DownloadRecords
   alias SwiftCollections.DownloadRecords.DownloadRecord
 
   describe "test sftp consuming" do
@@ -18,9 +19,11 @@ defmodule SwiftCollections.SftpFileDownloaderTest do
       {:ok, filename: filename, conn: conn, file_hash: file_hash, file_content: file_content}
     end
 
-    test "consumes message", context do
-      SwiftCollections.SftpFileDownloader.download_files()
-      :timer.sleep(250)
+    test "creates download record", context do
+      logs =
+        ExUnit.CaptureLog.capture_log(fn ->
+          SwiftCollections.SftpFileDownloader.download_files()
+        end)
 
       download_record = Repo.one!(DownloadRecord)
       {:ok, archived_files} = SFTPClient.list_dir(context.conn, "/archive")
@@ -29,7 +32,27 @@ defmodule SwiftCollections.SftpFileDownloaderTest do
       assert download_record.file_hash == context.file_hash
       assert download_record.content == context.file_content
 
-      assert Enum.any?(archived_files, fn el -> el == context.filename end)
+      assert Enum.any?(archived_files, &(&1 == download_record.filename))
+      assert logs =~ "Successfully created download record #{download_record.id}"
+    end
+
+    test "download record already exists", context do
+      {:ok, existing_record} =
+        DownloadRecords.Create.create(context.filename, context.file_content)
+
+      logs =
+        ExUnit.CaptureLog.capture_log(fn ->
+          SwiftCollections.SftpFileDownloader.download_files()
+        end)
+
+      download_record = Repo.one!(DownloadRecord)
+      {:ok, archived_files} = SFTPClient.list_dir(context.conn, "/archive")
+
+      assert download_record == existing_record
+      assert Enum.any?(archived_files, &(&1 == download_record.filename))
+
+      assert logs =~ "Failed to create download record"
+      assert logs =~ "Found duplicate record with hash #{download_record.file_hash}"
     end
   end
 end
